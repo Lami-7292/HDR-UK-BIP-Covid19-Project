7b4c9448ced63966412b5f9ee1dc81a04517c019
Lami-7292 <olajadenuga@gmail.com>
2025-07-24 11:54:18 +0100

    Plotting the Incidence & Cumulative Curves

--- Predicted vs Observed Sub-National Covid-19 Cases (UK).R
+++ Predicted vs Observed Sub-National Covid-19 Cases (UK).R
@@ -1,7 +1,7 @@
 # Inputting all the relevant libraries: STEP 1
-library(dplyr)     # for data manipulation (e.g. aggregating data)
+library(dplyr)   # for data manipulation (e.g. aggregating data)
+library(scales)   # better date formatting
 library(ggplot2)   # for plotting graphs
-library(scales)    # better date formatting
 library(digest)   # creates hash values for data, which is essential for caching, data verification and ensuring reproducibility
 
 
@@ -22,7 +22,7 @@ ggplot(predicted_data, aes(x = date, y = confirm)) +
   scale_x_date(date_labels = "%B %Y") +
   # modifies the x-axis to add month and year, not just have the month: %B = full month name and %Y = 4-digit year  
   scale_y_continuous(labels = comma) +   # modifies y-axis to make it more readable (e.g. from 2e+05 to 200,000) to show the actual integer
-  labs(title = "Predicted COVID-19 Cases in the United Kingdom",
+  labs(title = "Predicted COVID-19 Cases in The United Kingdom",
        x = "Date",
        y = "Number of Predicted Cases") +
   theme_classic()
@@ -36,16 +36,31 @@ observed_data$date <- as.Date(observed_data$date)    # converts the date (column
 observed_data <- observed_data[, c("date", "confirmed")]   # allows me to extract only the 2 variables (columns) that I needed from the data
 observed_data <- observed_data %>%
   rename(confirm = confirmed)
-plot(observed_data)
+plot(observed_data)   # the output was a cumulative plot
+
 
 # Transform the observed data from a cumulative plot to an incidence plot to match that of the predicted data:
 observed_data <- observed_data %>%
   arrange(date) %>%
-  mutate(incidence = c(NA, diff(confirm))) %>%
-  filter(!is.na(incidence)) %>%
-  select(date, incidence) %>%
-  rename(confirm = incidence)
-plot(observed_data)
+  mutate(confirm = confirm - lag(confirm)) %>%
+  mutate(confirm = ifelse(is.na(confirm), 0, confirm)) %>%   # replaces any NA values with 0
+  select(date, confirm)
+
+
+# Incidence curve produced when plotting observed data (just for checking how it look without overlaying) - just to check how the plot looks visually
+ggplot(observed_data, aes(x = date, y = confirm)) +
+  geom_line(color = "red", linewidth = 0.5) +
+  scale_x_date(date_labels = "%B %Y") +
+  scale_y_continuous(labels = comma) +
+  labs(
+    title = "Observed COVID-19 Cases in The United Kingdom",
+    x = "Date",
+    y = "Number of Observed Cases"
+  ) +
+  theme_classic()
+  
+
+
 
 # Overlay the predicted and observed cases for a comparison [INCIDENCE CURVE]: STEP 5
 predicted_data <- predicted_data %>% select(date, confirm)
@@ -58,15 +73,103 @@ combined_data <- bind_rows(predicted_data, observed_data)
 
 ggplot(combined_data, aes(x = date, y = confirm, color = source)) +
   geom_line(linewidth = 1) +
-  scale_color_manual(values = c("Predicted" = "slateblue", "Observed" = "red")) +
+  scale_color_manual(values = c("Predicted" = "slateblue", "Observed" = "orange")) +
   scale_x_date(date_labels = "%B %Y" ) +
   scale_y_continuous(labels = comma) +
   labs(title = "A Comparison of Predicted and Observed COVID-19 Cases In The UK",
        x = "Date",
        y = "Number of Cases",
-       color = "Case Category") + 
+       color = "Type of Case Data") + 
   theme_classic() +
   theme(legend.position = "top")
 
 # predicted data initially produces an incidence curve
 # observed data initially produces a cumulative curve
+
+
+
+
+# Overlay the predicted and observed cases for a comparison [CUMULATIVE CURVE]: STEP 6
+install.packages("COVID19")
+library("COVID19")
+observed_data <- covid19(country = "United Kingdom", level = 1, start = "2022-03-04", end = "2022-06-24")
+observed_data$date <- as.Date(observed_data$date)    # converts the date (column) variable into an actual Date (class) format
+observed_data <- observed_data[, c("date", "confirmed")]   # allows me to extract only the 2 variables (columns) that I needed from the data
+observed_data <- observed_data %>%
+  rename(confirm = confirmed)
+plot(observed_data)   # the output was a cumulative plot
+
+ggplot(observed_data, aes(x = date, y = confirm)) +
+  geom_line(color = "red", linewidth = 0.5) +
+  scale_x_date(date_labels = "%B %Y") +
+  scale_y_continuous(labels = comma) +
+  labs(
+    title = "Observed COVID-19 Cases in The United Kingdom",
+    x = "Date",
+    y = "Number of Observed Cases"
+  ) +
+  theme_classic()
+
+
+
+setwd("C:/Users/OlamideAdenuga/Desktop/COVID-19 Forecasts Internship Project/HDR-UK-BIP-Covid19-Project")
+getwd()
+predicted_data = read.csv("predicted_cases.csv")
+predicted_data <- predicted_data %>%
+  filter(region == "United Kingdom")   # filters the data for where the region is United Kingdom
+predicted_data$date <- as.Date(predicted_data$date)    # converts the date (column) variable into an actual Date (class) format
+predicted_data$region <- NULL   # region variable (column) is redundant and so can be removed using NULL
+predicted_data <- predicted_data[, c("date", "confirm")]   # allows me to extract only the 2 variables (columns) that I needed from the data
+
+# Plotting & formatting the line graph for the predicted data [incidence curve]: STEP 3
+ggplot(predicted_data, aes(x = date, y = confirm)) +
+  geom_line(color = "slateblue", linewidth = 0.5) +   #linewidth dictates the size or 'thickness' of the line on the plot
+  scale_x_date(date_labels = "%B %Y") +
+  # modifies the x-axis to add month and year, not just have the month: %B = full month name and %Y = 4-digit year  
+  scale_y_continuous(labels = comma) +   # modifies y-axis to make it more readable (e.g. from 2e+05 to 200,000) to show the actual integer
+  labs(title = "Predicted COVID-19 Cases in The United Kingdom",
+       x = "Date",
+       y = "Number of Predicted Cases") +
+  theme_classic()
+
+
+# Transform the predicted data from an incidence plot to an cumulative plot plot to match that of the observed data:
+predicted_data <- predicted_data %>%
+  arrange(date) %>%
+  mutate(confirm = cumsum(confirm)) %>%   # cumulative sum of incidence
+  select(date, confirm)
+
+
+
+ggplot(predicted_data, aes(x = date, y = confirm)) +
+  geom_line(color = "red", linewidth = 0.5) +
+  scale_x_date(date_labels = "%B %Y") +
+  scale_y_continuous(labels = comma) +
+  labs(
+    title = "Predicted COVID-19 Cases in The United Kingdom",
+    x = "Date",
+    y = "Number of Predicted Cases"
+  ) +
+  theme_classic()
+
+predicted_data <- predicted_data %>% select(date, confirm)
+observed_data <- observed_data %>% select(date, confirm)
+
+predicted_data$source <- "Predicted"
+observed_data$source <- "Observed"
+
+combined_data <- bind_rows(predicted_data, observed_data)
+
+ggplot(combined_data, aes(x = date, y = confirm, color = source)) +
+  geom_line(linewidth = 1) +
+  scale_color_manual(values = c("Predicted" = "slateblue", "Observed" = "orange")) +
+  scale_x_date(date_labels = "%B %Y" ) +
+  scale_y_continuous(labels = comma) +
+  labs(title = "A Comparison of Predicted and Observed COVID-19 Cases In The UK",
+       x = "Date",
+       y = "Number of Cases",
+       color = "Type of Case Data") + 
+  theme_classic() +
+  theme(legend.position = "top")
+
+
